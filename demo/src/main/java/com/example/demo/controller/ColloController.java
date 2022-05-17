42509d1be6abc46d36e94354bef28cc879dae4c5
StefanGarofalo <91844998+StefanGarofalo@users.noreply.github.com>
2022-05-17 15:22:16 +0200

    Add files via upload
    Controller con le route aggiornate per le get e le post per supportare i nuovi file html tra cui il form di edit

--- demo/src/main/java/com/example/demo/controller/ColloController.java
+++ demo/src/main/java/com/example/demo/controller/ColloController.java
@@ -5,16 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.naming.Binding;
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/colli/")
 public class ColloController {
+
+    private Long IdGlobale;
+
     @Autowired
     ColloRepository colloRepository;
 
@@ -25,16 +28,6 @@ public class ColloController {
         return "inserimento-colli";
     }
 
-    @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public String aggiungiCollo(@Valid Collo collo, BindingResult result, Model model){
-        if (result.hasErrors()){
-            return "inserimento-colli";
-        }
-        else{
-            colloRepository.save(collo);
-            return "redirect:../colli/";
-        }
-    }
     @GetMapping(value = { "/", ""})
     public String index(Model model){
         List<Collo> results = (List<Collo>) colloRepository.findAll();
@@ -49,9 +42,44 @@ public class ColloController {
         return "colli/show";
     }
 
+    @GetMapping(value = {"/edit/{id}"})
+    public String edit(@PathVariable("id") Long id, Model model){
+        Optional<Collo> collo = colloRepository.findById(id);
+        model.addAttribute("collo", collo.get());
+        IdGlobale = id;
+        return "colli/edit";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String modificaCollo(@Validated Collo newCollo, BindingResult result, Model model){
+        if(!colloRepository.findById(newCollo.getColliId()).equals(Optional.empty())){ 
+            Collo colloInDB = colloRepository.findById(newCollo.getColliId()).get();
+            colloInDB.setAltezza(newCollo.getAltezza());
+            colloInDB.setLarghezza(newCollo.getLarghezza());
+            colloInDB.setProfondita(newCollo.getProfondita());
+            colloInDB.setPeso(newCollo.getPeso());
+            colloInDB.setBolla(newCollo.getBolla());
+            colloRepository.save(colloInDB);
+
+            return "redirect:../colli/";
+        }
+        return "redirect:../colli/";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public String aggiungiCollo(@Validated Collo collo, BindingResult result, Model model){
+        if (result.hasErrors()){
+            return "inserimento-colli";
+        }
+        else{
+            colloRepository.save(collo);
+            return "redirect:../colli/";
+        }
+    }
+    
+
     @RequestMapping(value =("/delete/{id}"))
     public String delete(@PathVariable Long id){
-        //colloRepository.findById(id);
         if (!colloRepository.findById(id).equals(Optional.empty())) {
             colloRepository.deleteById(id);
         }
